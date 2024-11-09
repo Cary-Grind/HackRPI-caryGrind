0db29f275e1c4d2901461ecc42048c577f63e741
MattMRadford <radfom@rpi.edu>
2024-11-09 15:24:43 -0500

    Login and Signup Pages

---
+++ loo_locator/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <SignupForm />
+    <LogIn />
+  </div>
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue';
+import LogIn from './components/login.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SignupForm,
+    LogIn,
+  },
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<router-link to="/signup">Signup</router-link>
+<router-link to="/login">Login</router-link>
