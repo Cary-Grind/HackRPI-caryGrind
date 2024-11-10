c98701b7c4d9fff6af97bc90370c3a051813844a
MattMRadford <radfom@rpi.edu>
2024-11-09 20:18:16 -0500

    No longer pushing key

--- loo_locator/src/main.js
+++ loo_locator/src/main.js
@@ -1,17 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
 import VueGoogleMaps from '@fawmi/vue-google-maps';
 
-
-
-
+// Create and configure the Vue app
 const app = createApp(App);
+
+// Use the VueGoogleMaps plugin with the API key from the .env file
 app.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCF_zQA5quwiJ0Absat5w35Ly9Ch2H2MRw', // Replace with your API key
-    libraries: 'places', // Necessary if you use the Places API
+    key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY, // Load the API key from .env
+    libraries: 'places', // Load the 'places' library if needed
   },
 });
-app.mount('#app');
 
-  
+// Mount the app
+app.mount('#app');
